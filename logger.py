47c25c74f29c57381ccdb1cebf5b8913f3a7899b
invilso <40578604+invilso@users.noreply.github.com>
2022-06-28 00:11:45 +0300

    loglevel edit

--- logger.py
+++ logger.py
@@ -16,7 +16,7 @@ def get_stream_handler():
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     logger.addHandler(get_file_handler())
     logger.addHandler(get_stream_handler())
     return logger
